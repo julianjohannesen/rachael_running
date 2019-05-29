22da93932cafe8487e4239c262384a547c497c6b
julianjohannesen <github email address>
2019-05-29 11:54:16 -0400

    Added details to about page

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,9 +7,25 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="About" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1>Welcome</h1>
+    
+		<p>I am excited to be running for Malden School Committee in Ward 8. My daughter, Willa, is in 1st grade at Linden and my son, Luca, will be entering kindergarten there this fall. For me, the success of our schools is personal. But no matter who we are or what issues we face, everyone in Malden is touched by our schools and we all know that when our children succeed, Malden succeeds.
+		</p>
+		​
+
+		<h2>More about me</h2>
+
+		<p>As the daughter of two educators, I have always understood the power of teachers to change lives. My wife, Sarah, and I have been together for 20 years and we love living in Malden and being a part of the public school community. We are inspired by the other families we have met through Linden and the amazing teachers who are incredibly dedicated to our children's success. Our neighborhood schools are places where parents and family members from all walks of life can come together to build a better community and world, and I hope my work on the school committee will help strengthen the voices of parents, teachers and students alike.
+		</p>
+		​
+
+		<p>I am a proud union member (OPEIU Local 6) and have been a member of a union my entire working life. I believe in the power of unions to help create democratic workplaces and societies, and I bring this perspective to my campaign. I have dedicated my life to working for a better world through nonprofit policy, research, and communications work, and currently work as the communications coordinator for the Massachusetts AFL-CIO, which is the umbrella labor organization in the Commonwealth.
+		</p>
+		​
+
+		<p>I am proud to call Malden my home and I am excited to have the opportunity to earn your vote for School Committee in Ward 8. I welcome your ideas and input - please don’t hesitate to reach out. Together we can win for Malden! </p>
+
+		<p><Link to="/">Go back to the homepage</Link></p>
   </Layout>
 )
 
