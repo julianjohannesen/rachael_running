17b1961099d7ce92fdf2f6eb79836c84a98939d0
julianjohannesen <github email address>
2019-05-31 11:57:19 -0400

    added donate link

--- src/components/header.js
+++ src/components/header.js
@@ -18,7 +18,7 @@ const Header = ({ siteTitle }) => (
 		<Link to="/about/">About Rachael</Link>
 		<Link to="/issues/">Issues</Link>
 		<Link to="/get-involved/">Get Involved</Link>
-		<Link to="/">DONATE</Link>
+		<Link to="https://secure.actblue.com/donate/rachaelformalden">DONATE</Link>
 	</nav>
   </header>
 )
