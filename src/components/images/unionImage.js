0e1ead31849f64f1d792362b6a112241021870a0
julianjohannesen <github email address>
2019-06-03 11:29:22 -0400

    Working on issues page and media queries

--- src/components/images/unionImage.js
+++ src/components/images/unionImage.js
@@ -15,7 +15,13 @@ const UnionImage = () => (
         }
       }
     `}
-    render={data => <Img style={{maxWidth:'600xp', minWidth: '300px', }} imgStyle={{maxWidth: '600px', minWidth: '300px', objectFit: 'cover', }} fluid={data.placeholderImage.childImageSharp.fluid} />}
+    render={data => (
+      // margin 0 auto is to center the photo when dropping from side by side to above and below.
+      <Img 
+        style={{maxWidth:'600px', minWidth: '200px', margin: '0 auto'}} 
+        imgStyle={{maxWidth: '600px', minWidth: '200px', objectFit: 'cover', }} 
+        fluid={data.placeholderImage.childImageSharp.fluid} 
+      />)}
   />
 )
 export default UnionImage
