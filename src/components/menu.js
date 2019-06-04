ff61df2864b57a796a2df0830784feae5634af99
julianjohannesen <github email address>
2019-06-04 16:54:50 -0400

    new favicon, working on media queries for menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,18 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
 import cn from 'classnames'
-import { nav, link, donate, donateWrapper } from './menu.module.css'
+import { nav, menuWrapper, link, donate, donateWrapper } from './menu.module.css'
 
 
 const Menu = ({ classes = {} }) => {
 
 	return (
 		<nav className={cn(nav, classes.nav)}>
-			<div>
-				<Link className={cn(link, classes.link)} to="/">Home</Link>
-				<Link className={cn(link, classes.link)} to="/about/">About Rachael</Link>
-				<Link className={cn(link, classes.link)} to="/issues/">Issues</Link>
-				<Link className={cn(link, classes.link)} to="/get-involved/">Get Involved</Link>
+			<div className={cn(menuWrapper, classes.menuWrapper)}>
+				<Link className={cn(link, classes.link)} activeClassName="active" to="/">Home</Link>
+				<Link className={cn(link, classes.link)} activeClassName="active" to="/about/">About Rachael</Link>
+				<Link className={cn(link, classes.link)} activeClassName="active" to="/issues/">Issues</Link>
+				<Link className={cn(link, classes.link)} activeClassName="active" to="/get-involved/">Get Involved</Link>
 			</div>
 			<div className={cn(donateWrapper, classes.donateWrapper)}>
 				<a className={cn(donate, classes.donate)} href="https://secure.actblue.com/donate/rachaelformalden" title="donate">DONATE</a>
