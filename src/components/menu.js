1eba82d7e6742122e31673f7720ea6c15ef30028
julianjohannesen <github email address>
2019-06-05 10:53:28 -0400

    CSS tweaks to donate button in footer and phone input

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import cn from 'classnames'
-import { nav, menuWrapper, link, donate, donateWrapper } from './menu.module.css'
+import { nav, menuWrapper, link, donate, donateWrapper, active } from './menu.module.css'
 
 
 const Menu = ({ classes = {} }) => {
@@ -10,9 +10,9 @@ const Menu = ({ classes = {} }) => {
 		<nav className={cn(nav, classes.nav)}>
 			<div className={cn(menuWrapper, classes.menuWrapper)}>
 				<Link className={cn(link, classes.link)} activeClassName="active" to="/">Home</Link>
-				<Link className={cn(link, classes.link)} activeClassName="active" to="/about/">About Rachael</Link>
-				<Link className={cn(link, classes.link)} activeClassName="active" to="/issues/">Issues</Link>
-				<Link className={cn(link, classes.link)} activeClassName="active" to="/get-involved/">Get Involved</Link>
+				<Link className={cn(link, classes.link)} activeClassName="active" to="/about">About Rachael</Link>
+				<Link className={cn(link, classes.link)} activeClassName="active" to="/issues">Issues</Link>
+				<Link className={cn(link, classes.link)} activeClassName="active" to="/get-involved">Get Involved</Link>
 			</div>
 			<div className={cn(donateWrapper, classes.donateWrapper)}>
 				<a className={cn(donate, classes.donate)} href="https://secure.actblue.com/donate/rachaelformalden" title="donate">DONATE</a>
