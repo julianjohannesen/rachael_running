c5ba73674c3771ad221653499b3ce06f107ab251
julianjohannesen <julian.johannesen@gmail.com>
2021-04-07 12:01:32 -0400

    new build after plugin updates etc

--- src/pages/get-involved.js
+++ src/pages/get-involved.js
@@ -26,9 +26,8 @@ const GetInvolved = () => (
 					<li>Donate, either through the "donate" button above or by sending a check to: <address className={address}>The Committee to Elect Rachael Running<br/>51 Bellvale St, <br/>Malden, MA 02148.</address></li>
 				</ul>
 			</div>
-			<VolunteerForm formTitle={"I want to volunteer!"} />
+			{/* <VolunteerForm formTitle={"I want to volunteer!"} /> */}
 		</section>
-
 		
   </Layout>
 )
